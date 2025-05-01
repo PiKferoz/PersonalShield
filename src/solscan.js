cd1c20a6c4a11cb94fd2e3591c1f09ada95dda91
PiKferoz <diocarrazzone@gmail.com>
2025-05-01 17:03:01 -0300

    Update solscan.js

--- src/solscan.js
+++ src/solscan.js
@@ -1,1 +1,19 @@
+export default async function handler(req, res) {
+  const { endpoint, query } = req.query;
 
+  const url = `https://public-api.solscan.io/chaininfo/${endpoint}?${query}`;
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        accept: "application/json"
+      }
+    });
+
+    const data = await response.json();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Erro na proxy da Solscan" });
+  }
+}
