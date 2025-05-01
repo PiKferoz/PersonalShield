5fdb3ff310f734c216337ce5bd56a578f513dcb7
PiKferoz <diocarrazzone@gmail.com>
2025-05-01 20:18:38 -0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,16 @@
+// vite.config.mjs
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['file-saver'],
+    },
+  },
+  optimizeDeps: {
+    include: ['file-saver'],
+  },
 });
