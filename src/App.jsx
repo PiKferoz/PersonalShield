523046d9312cf658643f3d04a413c15294381a54
PiKferoz <diocarrazzone@gmail.com>
2025-05-01 21:04:27 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 // Tecnologias: React + Vite
 
 import React, { useState, useEffect } from "react";
-import { saveAs } from "./src/utils.js";
+import { saveAs } from "./utils/file-saver";
 import TokenScanner from "./TokenScanner";
 
 export default function App() {
